ea5b72c2f54160e4085458999d6a774f763473d6
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-11-26 22:04:43 +0200

    update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+// console.log(startBtn);
+const stopBtn = document.querySelector('button[data-stop]');
+// console.log(stopBtn);
+const body = document.querySelector('body');
+// console.log(body);
+
+let background = null;
+stopBtn.disabled = true;
+startBtn.addEventListener('click', onStartBtn);
+stopBtn.addEventListener('click', onStopBtn);
+
+
+function onStartBtn() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    background = setInterval(() => { body.style.backgroundColor = getRandomHexColor() }, 1000)
+}
+
+
+function onStopBtn () {
+     startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(background);
+}
