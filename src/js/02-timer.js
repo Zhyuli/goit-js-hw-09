6b69f2e952ab24a5af663c82ff13b6cb2afc1f14
Yulia Zhukova <132227187+Zhyuli@users.noreply.github.com>
2023-11-30 15:02:55 +0200

    update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -17,7 +17,9 @@ const startBtn = document.querySelector('button[data-start]');
 
 
 startBtn.disabled = true;
-startBtn.addEventListener('click', startCounter);
+startBtn.addEventListener('click', timerStart);
+
+let remainingTime = 0;
 
 const options = {
   enableTime: true,
@@ -26,23 +28,56 @@ const options = {
   minuteIncrement: 1,
 
   onClose(selectedDates) {
-    if (selectedDates[0].getTime() < Date.now()) {
-      Report.failure("Please choose a date in the future");
-    } else {
-      selectedDates = selectedDates[0].getTime();
-      startBtn.disabled = false;
-
-      Report.success('click start');
-    }
+    checkDate(selectedDates);
   },
 };
 
 flatpickr(timerInput, options);
 
 Report.info('Select the date and click on start');
+  
+function checkDate (selectedDates) {
+  selectedDate = selectedDates[0].getTime();
+  const currentDate = new Date().getTime();
 
-function startCounter () {
-  counter.start();
+if (selectedDate > currentDate) {
+  startBtn.disabled = false;
+  Report.success('Click on start!');
+  return;
+}
+Report.failure('Please, choose a date in the future');
+}
+
+function timerStart() {
+  intervalId = setInterval(() => {
+    currentDate = new Date().getTime();
+
+    if (selectedDate - currentDate <= 1000) {
+      clearInterval(intervalId);
+      startBtn.disabled = true;
+      timerInput.disabled = false;
+      Report.info('Timer stopped!');
+      return;
+    } else {
+      startBtn.disabled = true;
+      timerInput.disabled = true;
+      currentDate += 1000;
+      remainingTime = Math.floor(selectedDate - currentDate);
+      convertMs(remainingTime);
+    }
+  }, 1000);
+}
+
+function createMarkup ({days, hours, minutes, seconds}) {
+  timerDays.textContent = days;
+  timerHours.textContent = hours;
+  timerMinutes.textContent = minutes;
+  timerSeconds.textContent = seconds;
+}
+
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
 }
 
 function convertMs(ms) {
@@ -52,54 +87,19 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
+ 
   const days = addLeadingZero(Math.floor(ms / day));
-  // Remaining hours
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  // Remaining minutes
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  // Remaining seconds
   const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
 
+  createMarkup ({days, hours, minutes, seconds})
   return { days, hours, minutes, seconds };
 }
 
-const counter = {
-  start() {
-    intervalId = setInterval(() => {
-      currentDate = Date.now();
-      const deltaTime = selectedDate - currentDate;
-      updateTimer(convertMs(deltaTime));
-      startBtn.disabled = true;
-      timerInput.disabled = true;
-
-      if (deltaTime <= 1000) {
-        this.stop();
-        Report.info('Timer stopped');
-      }
-    }, 1000);
-  },
-  
-  stop() {
-    startBtn.disabled = true;
-    timerInput.disabled = false;
-    clearInterval(intervalId);
-    return;
-  },
-};
-
-function updateTimer({days, hours, minutes, seconds}) {
-  timerDays.textContent = `${days}`;
-  timerHours.textContent = `${hours}`;
-  timerMinutes.textContent = `${minutes}`;
-  timerSeconds.textContent = `${seconds}`;
-}
-
-function addLeadingZero(value) {
-     return String(value).padStart(2, '0');
-  }
 
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+ 
+
+
+
